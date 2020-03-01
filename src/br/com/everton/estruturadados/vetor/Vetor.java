b7f83f42ec3dacce6b5e6653916ac62f93130ddd
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-03-01 20:18:41 -0300

    Aula 03 - Vetores e Arrays: Adicionar elemento no final do vetor

--- src/br/com/everton/estruturadados/vetor/Vetor.java
+++ src/br/com/everton/estruturadados/vetor/Vetor.java
@@ -10,24 +10,21 @@ public class Vetor {
 		this.tamanho = 0;
 	}
 
-	/*public void adiciona(String elemento) {
-		for (int i = 0; i < this.elementos.length; i++) {
-			if (elementos[i] == null) {
-				this.elementos[i] = elemento;
-				break;
-			}
-		}
-	}*/
+	/*
+	 * public void adiciona(String elemento) { for (int i = 0; i <
+	 * this.elementos.length; i++) { if (elementos[i] == null) { this.elementos[i] =
+	 * elemento; break; } } }
+	 */
 
 	/*
-	 * public void adiciona(String elemento) throws Exception { if(this.tamanho <
+	 * public void adiciona(String elemento) throws Exception { if (this.tamanho <
 	 * this.elementos.length) { this.elementos[this.tamanho] = elemento;
 	 * this.tamanho++; } else { throw new
 	 * Exception("Vetor já está cheio, não é possível adicionar mais elementos"); }
 	 * }
 	 */
 
-	public boolean adicionar(String elemento) throws Exception {
+	public boolean adiciona(String elemento) throws Exception {
 		if (this.tamanho < this.elementos.length) {
 			this.elementos[this.tamanho] = elemento;
 			this.tamanho++;
