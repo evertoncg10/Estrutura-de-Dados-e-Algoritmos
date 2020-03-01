ad63430bf1433305ff463c361d876fff8c8f9785
Everton Cezar Gonçalves <evertoncg10@outlook.com>
2020-03-01 19:20:47 -0300

    Aula 02 - Vetores e Arrays: Introdução

--- src/br/com/everton/estruturadados/vetor/Vetor.java
+++ src/br/com/everton/estruturadados/vetor/Vetor.java
@@ -2,4 +2,10 @@ package br.com.everton.estruturadados.vetor;
 
 public class Vetor {
 
+	private String[] elementos;
+	
+
+	public Vetor(int capacidade) {
+		this.elementos = new String[capacidade];
+	}
 }
---
+++ src/br/com/everton/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,13 @@
+package br.com.everton.estruturadados.vetor.teste;
+
+import br.com.everton.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+
+	}
+
+}
